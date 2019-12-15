f590ee8ad9f72c3de036085477ebed104ac7b58a
dar-code <56978829+dar-code@users.noreply.github.com>
2019-12-14 22:37:19 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,13 +56,45 @@ showPage(listItems, 1);
 
 const appendPageLinks = (list) => {
    
-   //Variable pagesNeeded will included the rounded number of needed pages. I've used Math.ceil to round the number up to the next highest whole number (if we have 54 students, we should have 6 pages instead of 5). 
+   //Variable pagesCount will included the rounded number of needed pages. I've used Math.ceil to round the number up to the next highest whole number (if we have 54 students, we should have 6 pages instead of 5). 
 
-   const pagesNeeded = Math.ceil(list.length / perPage);
+   const pagesCount = Math.ceil(list.length / perPage);
+   
+   //Created a div element within a variable called 'div', stored it in the .page, and added a class 'pagination' to it.
 
+   const div = document.createElement('div');
+   const pageElement = document.querySelector('.page');
+   pageElement.appendChild(div);
+   div.className = 'pagination';
 
+   //Appended ul to the 'pagination' class div.
 
-}
+   const ul = document.createElement('ul');
+   div.appendChild(ul);
+
+   for (let i = 0; i < pageElement.length; i++) {
+      const ul = div.getElementsByTagName('ul')[0];
+      const a = createElement('a');
+      const li = createElement('li');
+
+      li.appendChild(a);
+      ul.appendChild(li);
+
+      a.href = '#';
+      a.textContent = i + 1;
+   }
+
+      a.addEventListener('click', (e) => {
+         let pageToShow = parseInt(e.target.textContent);
+         const pageLinks = document.querySelectorAll('a');
+         for (let i = 0; i < pageLinks.length) {
+            pageLinks[i].classList.remove('active');
+         }
+         e.target.className = 'active';
+         showPage(list, pageToShow);
+      });
+   };
+
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
