9631b7535ac8506d694a517d1582dfcecdfe239f
dar-code <56978829+dar-code@users.noreply.github.com>
2020-03-12 23:22:26 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,50 @@
 // Variables
 
-const itemParent = document.querySelector('.student-list');
+const fullList = document.querySelector('.student-item');
 const listItems = itemParent.children;
+const page = document.querySelector('.page');
 const perPage = 10;
+const currentPage = 1;
 
 // ShowPage Function
 
 const showPage = (list, page) => {
-   const startIndex = (listItems * perPage) - perPage;
-   const endIndex = listItems * perPage;
+   const startIndex = (page * perPage) - perPage;
+   const endIndex = page * perPage;
     for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i <= endIndex) {
-        list[i].style.color = 'block';
+         list[i].style.display = '';
       } else {
-         list[i].style.color = 'none'; 
+         list[i].style.display = 'none'; 
       }
    };
 
 };
 
+
+
+//Creating the pagination links dynamically
+
 const appendPageLinks = (list) => {
    
-   //Variable pagesCount will included the rounded number of needed pages. I've used Math.ceil to round the number up to the next highest whole number (if we have 54 students, we should have 6 pages instead of 5). 
+//Variable pagesCount will included the rounded number of needed pages. I've used Math.ceil to round the number up to the next highest whole number (if we have 54 students, we should have 6 pages instead of 5). 
 
-   const pagesCount = Math.ceil(list.length / perPage);
+   const pageCount = Math.ceil(list.length / perPage);
    
-   //Created a div element within a variable called 'div', stored it in the .page, and added a class 'pagination' to it.
+//Created a div element within a variable called 'div', stored it in the .page, and added a class 'pagination' to it.
 
-   const div = document.createElement('div');
-   const pageElement = document.querySelector('.page');
-   pageElement.appendChild(div);
-   div.className = 'pagination';
+   const newDiv = document.createElement('div');
+   page.appendChild(newDiv);
+   newDiv.className = 'pagination';
 
-   //Appended ul to the 'pagination' class div.
+//Appended ul to the 'pagination' class div.
 
    const ul = document.createElement('ul');
-   div.appendChild(ul);
+   newDiv.appendChild(ul);
+   
+//Created 'a' and 'li' elements
 
-   for (let i = 0; i < pageElement.length; i++) {
+   for (let i = 0; i < pageCount; i++) {
       const ul = div.getElementsByTagName('ul')[0];
       const a = createElement('a');
       const li = createElement('li');
@@ -45,20 +52,22 @@ const appendPageLinks = (list) => {
       li.appendChild(a);
       ul.appendChild(li);
 
-      a.href = '#';
+      const pageLinks = document.querySelectorAll('a');
+
+      a.setAttribute = ("href", "#");
+      if (i===0){
+         a.classList = 'active';
+      }
       a.textContent = i + 1;
    }
 
-      a.addEventListener('click', (e) => {
-         let pageToShow = parseInt(e.target.textContent);
-         const pageLinks = document.querySelectorAll('a');
-         for (let i = 0; i < pageLinks.length; i++) {
-            pageLinks[i].classList.remove('active');
-         }
-         e.target.className = 'active';
-        
-      });
-   };
+//Created the click event listener
 
-
-   showPage(list, pageToShow);
+      for (let i = 0; i < pageLinks.length; i++) {
+            pageLinks[i].addEventListener('click', (e) =>{
+               pageLinks[i].classList.remove('active');
+               currentPage = e.target.textContent;
+               e.target.className = 'active';
+               showPage(fullList, 1);
+            )}
+};
